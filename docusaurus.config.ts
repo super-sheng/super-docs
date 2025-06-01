cb3b957fc942529700d9d9ec1206200e3e1194cd
capricorn-ws <max.capricorn1209@gmail.com>
2025-06-01 22:05:30 +0800

    feat：v1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/super-docs/',
-
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'super-sheng', // Usually your GitHub org/user name.
